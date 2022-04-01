c611956e3298a1c3c1582477283c5550812e97f5
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-01 22:11:41 +0300

    Add playRound function

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 function computerPlay() {
-    let arr = ['rock', 'paper', 'scissors']
+    let arr = ['rock', 'paper', 'scissors'];
     return arr[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay());
+function playRound(playSel, compSel) {
+    console.log(playSel);
+    console.log(compSel);
+
+    if (playSel === 'rock' && compSel === 'paper') {
+        return `You lose: ${compSel} beats ${playSel}!`;
+    }
+    else if (playSel === 'paper' && compSel === 'scissors') {
+        return `You lose: ${compSel} beats ${playSel}!`;
+    }
+    else if (playSel === 'scissors' && compSel === 'rock') {
+        return `You lose: ${compSel} beats ${playSel}!`;
+    }
+    else if (playSel === 'rock' && compSel === 'scissors') {
+        return `You win: ${playSel} beats ${compSel}!`;
+    }
+    else if (playSel === 'paper' && compSel === 'rock') {
+        return `You win: ${playSel} beats ${compSel}!`;
+    }
+    else if (playSel === 'scissors' && compSel === 'paper') {
+        return `You win: ${playSel} beats ${compSel}!`;
+    }
+    else if (playSel === 'rock' && compSel === 'rock') {
+        return `It's a tie, you both selected ${compSel}!`;
+    }
+    else if (playSel === 'paper' && compSel === 'paper') {
+        return `It's a tie, you both selected ${compSel}!`;
+    }
+    else if (playSel === 'scissors' && compSel === 'scissors') {
+        return `It's a tie, you both selected ${compSel}!`;
+    }
+    else {
+        return 'You did not type one of the mentioned words!'
+    }
+
+}
+
+const playerSelection = prompt('Select Rock, Paper or Scissors!').toLowerCase();
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
