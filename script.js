699ce4f36f9bbcb647743c71afd32ae7bcd8b5b0
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-01 22:31:55 +0300

    Create game function and update playRound function

--- script.js
+++ script.js
@@ -8,39 +8,71 @@ function playRound(playSel, compSel) {
     console.log(compSel);
 
     if (playSel === 'rock' && compSel === 'paper') {
-        return `You lose: ${compSel} beats ${playSel}!`;
+        return 2;
     }
     else if (playSel === 'paper' && compSel === 'scissors') {
-        return `You lose: ${compSel} beats ${playSel}!`;
+        return 2;
     }
     else if (playSel === 'scissors' && compSel === 'rock') {
-        return `You lose: ${compSel} beats ${playSel}!`;
+        return 2;
     }
     else if (playSel === 'rock' && compSel === 'scissors') {
-        return `You win: ${playSel} beats ${compSel}!`;
+        return 1;
     }
     else if (playSel === 'paper' && compSel === 'rock') {
-        return `You win: ${playSel} beats ${compSel}!`;
+        return 1;
     }
     else if (playSel === 'scissors' && compSel === 'paper') {
-        return `You win: ${playSel} beats ${compSel}!`;
+        return 1;
     }
     else if (playSel === 'rock' && compSel === 'rock') {
-        return `It's a tie, you both selected ${compSel}!`;
+        return 0;
     }
     else if (playSel === 'paper' && compSel === 'paper') {
-        return `It's a tie, you both selected ${compSel}!`;
+        return 0;
     }
     else if (playSel === 'scissors' && compSel === 'scissors') {
-        return `It's a tie, you both selected ${compSel}!`;
+        return 0;
     }
     else {
-        return 'You did not type one of the mentioned words!'
+        return -1;
     }
 
 }
 
-const playerSelection = prompt('Select Rock, Paper or Scissors!').toLowerCase();
-const computerSelection = computerPlay();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
 
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt(`Select Rock, Paper or Scissors for the ${i + 1}. game!`).toLowerCase();
+        const computerSelection = computerPlay();
+        let res = playRound(playerSelection, computerSelection);
+
+        if (playerScore == 3 || computerScore == 3) {
+            break;
+        }
+        else if (res < 0) {
+            alert("You didn't type one of the words from the listed options!");
+        }
+        else if (res == 1) {
+            playerScore++;
+        }
+        else if (res == 2) {
+            computerScore++;
+        }
+        else if (res == 0) {
+            playerScore += 0;
+            computerScore += 0;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        alert(`You won with ${playerScore} points! The computer had only ${computerScore} points.`);
+    }
+    else {
+        alert(`You lost with ${playerScore} points! The computer won with ${computerScore} points.`);
+    }
+}
+
+game();
