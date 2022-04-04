e02876a79758dc3a4a6aabf2c1088035bfbd89be
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-04 18:54:58 +0300

    Add three buttons for rps, add event listeners to them

--- script.js
+++ script.js
@@ -1,70 +1,88 @@
 function computerPlay() {
-    let arr = ['rock', 'paper', 'scissors'];
-    return arr[Math.floor(Math.random() * 3)];
+	let arr = ['rock', 'paper', 'scissors'];
+	return arr[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playSel, compSel) {
-    if (playSel === 'rock' && compSel === 'paper') {
-        return 2;
-    }
-    else if (playSel === 'paper' && compSel === 'scissors') {
-        return 2;
-    }
-    else if (playSel === 'scissors' && compSel === 'rock') {
-        return 2;
-    }
-    else if (playSel === 'rock' && compSel === 'scissors') {
-        return 1;
-    }
-    else if (playSel === 'paper' && compSel === 'rock') {
-        return 1;
-    }
-    else if (playSel === 'scissors' && compSel === 'paper') {
-        return 1;
-    }
-    else if (playSel === 'rock' && compSel === 'rock') {
-        return 0;
-    }
-    else if (playSel === 'paper' && compSel === 'paper') {
-        return 0;
-    }
-    else if (playSel === 'scissors' && compSel === 'scissors') {
-        return 0;
-    }
-    else {
-        return -1;
-    }
+	if (playSel === 'rock' && compSel === 'paper') {
+		return 2;
+	}
+	else if (playSel === 'paper' && compSel === 'scissors') {
+		return 2;
+	}
+	else if (playSel === 'scissors' && compSel === 'rock') {
+		return 2;
+	}
+	else if (playSel === 'rock' && compSel === 'scissors') {
+		return 1;
+	}
+	else if (playSel === 'paper' && compSel === 'rock') {
+		return 1;
+	}
+	else if (playSel === 'scissors' && compSel === 'paper') {
+		return 1;
+	}
+	else if (playSel === 'rock' && compSel === 'rock') {
+		return 0;
+	}
+	else if (playSel === 'paper' && compSel === 'paper') {
+		return 0;
+	}
+	else if (playSel === 'scissors' && compSel === 'scissors') {
+		return 0;
+	}
+	else {
+		return -1;
+	}
 
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+function game(playerSelection) {
+	let playerScore = 0;
+	let computerScore = 0;
 
-    const playerSelection = prompt(`Select Rock, Paper or Scissors`).toLowerCase();
-    const computerSelection = computerPlay();
-    let res = playRound(playerSelection, computerSelection);
+	const computerSelection = computerPlay();
+	let res = playRound(playerSelection, computerSelection);
 
-    console.table(playerSelection, computerSelection);
+	console.table(playerSelection, computerSelection);
 
-    if (res == 1) {
-        playerScore++;
-    }
-    else if (res == 2) {
-        computerScore++;
-    }
+	if (res == 1) {
+		playerScore++;
+	}
+	else if (res == 2) {
+		computerScore++;
+	}
 
-    if (playerScore > computerScore) {
-        alert(`You won with ${playerScore} points! The computer only had
-                ${computerScore} points.`);
-    }
-    else if (playerScore < computerScore) {
-        alert(`You lost with ${playerScore} points! The computer won with
-                ${computerScore} points.`);
-    }
-    else {
-        alert(`It was a tie, you both picked ${playerSelection}`)
-    }
+	// if (playerScore > computerScore) {
+	//     alert(`You won with ${playerScore} points! The computer only had
+	//             ${computerScore} points.`);
+	// }
+	// else if (playerScore < computerScore) {
+	//     alert(`You lost with ${playerScore} points! The computer won with
+	//             ${computerScore} points.`);
+	// }
+	// else {
+	//     alert(`It was a tie, you both picked ${playerSelection}`)
+	// }
 }
 
-game();
+game('rock');
+
+// const container = document.querySelector('.container');
+const buttons = document.querySelectorAll('button');
+// console.log(buttons);
+
+buttons.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		if (e.target.className === 'btn-rock') {
+			game('rock');
+		}
+		else if (e.target.className === 'btn-paper') {
+			game('paper')
+		}
+		else if (e.target.className === 'btn-scissors') {
+			game('scissors')
+		}
+	});
+});
+
